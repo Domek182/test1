7e4501c4484b5d325e5f007ffc290af7be1aabd9
Domek182 <112411753+Domek182@users.noreply.github.com>
2024-01-16 15:02:27 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,73 @@
-#include "trojkat.h"
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 int main()
 {
-    Trojkat ABC;
-    cout<<"Podaj wspolrzedne punktu A\n";
-    cin>>ABC.A.x;
-    cin>>ABC.A.y;
-    cout<<"Podaj wspolrzedne punktu B\n";
-    cin>>ABC.B.x;
-    cin>>ABC.B.y;
-    cout<<"Podaj wspolrzedne punktu C\n";
-    cin>>ABC.C.x;
-    cin>>ABC.C.y;
-
-   if (PoleTrojkata(ABC) == -1){
-        printf("Z takich punktow nie mozna zbudowac trojkata");
-        return 0;
+    srand( time( NULL ) );
+    int NaSwoimMiejscu;
+    int NieNaSwoimMiejscu;
+    int n;
+    cout<<"Podaj dlugosc szyfru: ";
+    cin>>n;
+    int SZYFR[n];
+    int ODGADNIECIE[n];
+    for (int i=0; i<n; i++){
+        SZYFR[i]=rand()%10;
+        cout<<SZYFR[i];
     }
-   printf("Pole trojkata wynosi: %f", PoleTrojkata(ABC));
 
+    do{
+        int tab[n];
+        for (int i=0; i<n; i++){
+            tab[i] = -1;
+        }
+        NaSwoimMiejscu = 0;
+        NieNaSwoimMiejscu = 0;
+        cout<<"Sprobuj zgadnac szyfr: \n";
+        for (int i=0; i<n; i++){
+            cin>>ODGADNIECIE[i];
+        }
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if((ODGADNIECIE[j]==SZYFR[i])&& (tab[i]==-1)){
+                    NieNaSwoimMiejscu++;
+                    tab[i] = SZYFR[i];
+                    break;
+                }
+            }
+                 if (ODGADNIECIE[i]==SZYFR[i]){
+                        NaSwoimMiejscu++;
+                       // tab[i] = SZYFR[i];
+                        }
+
+
+
+        }
+
+    cout<<"Cyfry w szyfsze na swoim miejscu: "<<NaSwoimMiejscu<<"\n";
+    cout<<"Cyfry w szyfsze nie na swoim miejscu: "<<NieNaSwoimMiejscu<<"\n";
+
+    }
+    while(NaSwoimMiejscu != n);
+    cout<<"Udalo ci sie odgadnac szyfr!";
+
+
+
+
+
+
+
+
+
+   /* for (int i=0; i < n; i++)
+      cout << "LOS[" << i <<"]=" << LOS[i] << endl;
+
+    cout << "Wylosowanie pierwsze: " << rand()%10 << endl;
+    int liczba = rand();
+    cout << "Wylosowanie drugie: " << liczba%10 << endl;
+    liczba = rand();
+    cout << "Wylosowanie trzecie: " << liczba%10 << endl;
+    return 0;
+    // */
 }
-
